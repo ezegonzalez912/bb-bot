8ba1af3e346829a12d0bd07448bb1e5767d55ab0
ezegonzalez912 <ezegonzalez912@gmail.com>
2022-09-26 20:26:14 -0300

    dsadas

--- src/getMessage.js
+++ src/getMessage.js
@@ -5,7 +5,7 @@ const select = require ('puppeteer-select');
 const getMessages = async ({ url, msg, id }) => {
 
    //incia la pagina
-   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-extensions'] });
    const page = await browser.newPage();
    await page.goto(url);
    
